56f56536d2c8ceae45767b42fa53dbfdf9096183
ScislakKacper <kacpersci7@gmail.com>
2026-02-12 10:05:25 +0100

    Encja bazy danych

---
+++ app/src/main/java/com/lusia/room/Wypiek.java
@@ -0,0 +1,66 @@
+package com.lusia.room;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+// Dodajemy informację, że klasa jest encją (tabelą) bazy danych i podajemy jej nazwę
+@Entity (tableName = "wypieki tabela")
+public class Wypiek {
+    @PrimaryKey (autoGenerate = true) // samo sie bedzie generowac
+    private int id;
+    @ColumnInfo (name = "temperatura pieczenia") // nazwa kolumny
+    private int temperaturaPieczenia;
+    @ColumnInfo (name = "czas pieczenia")
+    private int czasPieczenia;
+    private String nazwaWypieku;
+    private String skladniki;
+
+    public Wypiek(int temperaturaPieczenia, int czasPieczenia, String nazwaWypieku, String skladniki) {
+        id = 0; // musi być
+        this.temperaturaPieczenia = temperaturaPieczenia;
+        this.czasPieczenia = czasPieczenia;
+        this.nazwaWypieku = nazwaWypieku;
+        this.skladniki = skladniki;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTemperaturaPieczenia() {
+        return temperaturaPieczenia;
+    }
+
+    public void setTemperaturaPieczenia(int temperaturaPieczenia) {
+        this.temperaturaPieczenia = temperaturaPieczenia;
+    }
+
+    public int getCzasPieczenia() {
+        return czasPieczenia;
+    }
+
+    public void setCzasPieczenia(int czasPieczenia) {
+        this.czasPieczenia = czasPieczenia;
+    }
+
+    public String getNazwaWypieku() {
+        return nazwaWypieku;
+    }
+
+    public void setNazwaWypieku(String nazwaWypieku) {
+        this.nazwaWypieku = nazwaWypieku;
+    }
+
+    public String getSkladniki() {
+        return skladniki;
+    }
+
+    public void setSkladniki(String skladniki) {
+        this.skladniki = skladniki;
+    }
+}
